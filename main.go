fc9f2eccff220afb79d7468a6ae7f75162419779
Amber Mani <ambermani@AmberManis-MacBook-Pro.local>
2021-04-21 19:42:34 +0530

    test mux

--- main.go
+++ main.go
@@ -1,29 +1,66 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
+	_ "github.com/heroku/x/hmetrics/onload"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func NewRouter() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Choose the folder to serve
+	staticDir := "/static/"
+
+	// Create the route
+	router.
+		PathPrefix(staticDir).
+		Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir))))
+
+	return router
+}
+//
+//func main() {
+//	port := "8080"
+//
+//	if port == "" {
+//		log.Fatal("$PORT must be set")
+//	}
+//
+//	router := gin.New()
+//	router.Use(gin.Logger())
+//	router.LoadHTMLGlob("templates/*.tmpl.html")
+//	router.Static("/static", "static")
+//
+//	router.GET("/", func(c *gin.Context) {
+//		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+//	})
+//	router.GET("/mark", func(c *gin.Context) {
+//		c.String(http.StatusOK, "testMark")
+//	})
+//
+//
+//	router.Run(":" + port)
+//	//handleRequests()
+//}
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
+	router := NewRouter()
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal("ListenAndServe Error: ", err)
 	}
-
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
-
-	router.Run(":" + port)
 }
